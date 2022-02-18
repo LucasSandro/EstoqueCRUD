e2aa1a867b4dc55ff4264d085600523d1f1b400f
LucasSandro <50306593+LucasSandro@users.noreply.github.com>
2022-02-17 21:58:24 -0300

    Removing content form AppPrincipal
    The content removed will go to the TelaPrincipal because this class(AppPrincipal) is used as an main for the program

--- AtividadeExtraclasseBD/Programa/AppPrincipal.py
+++ AtividadeExtraclasseBD/Programa/AppPrincipal.py
@@ -2,31 +2,6 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from TelaPrincipal import *
 
-class TelaPrincipal(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-
-        self.ui.setupUi(self)
-
-        self.ui.btnPesquisar.clicked.connect(self.pesquisa)
-        self.ui.btnAdicionar.clicked.connect(self.adiciona)
-        self.ui.btnAtualizar.clicked.connect(self.atualiza)
-        self.ui.btnRemover  .clicked.connect(self.remove  )
-    
-    def pesquisa(self):
-        print("Pesquisa clicado!")
-    
-    def adiciona(self):
-        print("Adiciona clicado!")
-
-    def atualiza(self):
-        print("Atualiza clicado!")
-
-    def remove(self):
-        print("Remove clicado!")
-
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = TelaPrincipal()
